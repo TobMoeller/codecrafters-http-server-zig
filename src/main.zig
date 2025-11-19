221adb75ba3ffb1ceb5ec59859cd382c7c22404b
TobMoeller <tobiasmoellerw@t-online.de>
2025-11-19 22:17:27 +0100

    init

--- src/main.zig
+++ src/main.zig
@@ -6,12 +6,12 @@ pub fn main() !void {
     std.debug.print("Logs from your program will appear here!\n", .{});
 
     // TODO: Uncomment the code below to pass the first stage
-    // const address = try net.Address.resolveIp("127.0.0.1", 4221);
-    // var listener = try address.listen(.{
-    //     .reuse_address = true,
-    // });
-    // defer listener.deinit();
-    //
-    // _ = try listener.accept();
-    // std.debug.print("client connected!", .{});
+    const address = try net.Address.resolveIp("127.0.0.1", 4221);
+    var listener = try address.listen(.{
+        .reuse_address = true,
+    });
+    defer listener.deinit();
+
+    _ = try listener.accept();
+    std.debug.print("client connected!", .{});
 }
